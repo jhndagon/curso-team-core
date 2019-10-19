f76d5c1bab18df4aeb59b808b1593a12971c24ea
John David Gonzalez <jhndagon@gmail.com>
2019-10-19 12:59:26 -0500

    Creacion pagina basica de estudiantes, con eliminar metodo eliminar.

---
+++ TGL/TGLWebApp/Data/StudentStore.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TGLWebApp.Models;
+
+namespace TGLWebApp.Data
+{
+    public class StudentStore
+    {
+        private List<Student> Students { get; set; } = new List<Student>();
+
+        public StudentStore()
+        {
+            Students.Add(new Student
+            {
+                Id= Guid.NewGuid(),
+                Age = 24,
+                Name = "John",
+                LastName = "David",
+                Nit = "12341234"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 24,
+                Name = "Pedro",
+                LastName = "Andres",
+                Nit = "432134"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 24,
+                Name = "Alguien",
+                LastName = "En serio",
+                Nit = "235423"
+            });
+        }
+
+        internal void DeleteStudent(Guid id)
+        {
+            var student = Students.FirstOrDefault(x => x.Id == id);
+            Students.Remove(student);
+        }
+
+        public List<Student> GetStudents()
+        {
+            return Students;
+        }
+    }
+}
